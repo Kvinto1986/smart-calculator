df3f884ae70f774836f69c3b458e63561820d86b
Kvinto1986 <kvinto1986@gmail.com>
2018-10-04 19:55:02 +0300

    up index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,85 @@
 class SmartCalculator {
     constructor(initialValue) {
-        this.val=initialValue
+        this.signs = [];
+        this.val = [];
+        this.val.push(initialValue)
     }
 
     add(number) {
-      this.val=this.val+'+'+number
+        this.signs.push('+');
+        this.val.push(number);
         return this
     }
 
     subtract(number) {
-        this.val=this.val+'-'+number
+        this.signs.push('-');
+        this.val.push(number);
         return this
     }
 
     multiply(number) {
-        this.val=this.val+'*'+number
+        this.signs.push('*');
+        this.val.push(number);
         return this
     }
 
     devide(number) {
-        this.val=this.val+'/'+number
+        this.signs.push('/');
+        this.val.push(number);
         return this
     }
 
     pow(number) {
-        this.val=this.val+'**'+number
+        this.signs.push('**');
+        this.val.push(number);
         return this
     }
 
+    valueOf() {
 
-    toString()
-    {
-      return eval(this.val)
+        while (this.signs.length > 0) {
+            for (let i = 0; i < this.signs.length; i++) {
+                if (this.signs[i] === '**' && this.signs[i + 1] !== '**') {
+                    this.val[i] = Math.pow(this.val[i], this.val[i + 1]);
+                    this.signs.splice(i, 1)
+                    this.val.splice(i + 1, 1)
+                    break
+
+                }
+                if (!this.signs.includes('**')) {
+                    if (this.signs[i] === '*') {
+                        this.val[i] = this.val[i] * this.val[i + 1];
+                        this.signs.splice(i, 1)
+                        this.val.splice(i + 1, 1)
+                        break
+                    }
+                    if (this.signs[i] === '/') {
+                        this.val[i] = this.val[i] / this.val[i + 1];
+                        this.signs.splice(i, 1)
+                        this.val.splice(i + 1, 1)
+                        break
+                    }
+                }
+                if (!this.signs.includes('**') && !this.signs.includes('*') && !this.signs.includes('/')) {
+
+                    if (this.signs[i] === '+') {
+                        this.val[0] += this.val[i + 1];
+                        this.signs.splice(i, 1)
+                        this.val.splice(i + 1, 1)
+                        break
+                    }
+                    if (this.signs[i] === '-') {
+                        this.val[0] -= this.val[i + 1];
+                        this.signs.splice(i, 1)
+                        this.val.splice(i + 1, 1)
+                        break
+                    }
+                }
+            }
+
+        }
+
+        return this.val[0];
     }
 }
 module.exports = SmartCalculator;
